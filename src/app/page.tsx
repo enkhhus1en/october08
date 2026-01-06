9d70a478f6f34cf5eb97da600df4b250fc6877b0
enkhhus1en <enkhhuslenbatmunkh@gmail.com>
2026-01-06 11:29:06 +0800

    embed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,18 @@ export default function Home() {
         <br />
         2026
       </div>
+      <div className="flex justify-center items-center mt-20">
+        <iframe
+          className="border-0"
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/nRTBM4UwnBs?si=MBJgfOWiDydETlSE"
+          title="YouTube video player"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+        ></iframe>
+      </div>
     </div>
   );
 }
